f1c9bd32cc64525fa556e9078e9f92971c5f669b
745333608 <745333608@qq.com>
2024-02-05 17:54:32 -0500

    no message

--- token.cpp
+++ token.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
+class tokeninfo
+{
+public:
+	tokeninfo();
+	~tokeninfo();
+	char* token[20];
+	int linecount;
+	int offset;
+private:
+
+};
+
+tokeninfo::tokeninfo()
+{
+	linecount = 0;
+	offset = 0;
+}
+
+tokeninfo::~tokeninfo()
+{
+}
+
+tokeninfo& getToken(fileobj,tokeninfo &buffer) {
+
+}
+
 int main(int argc, char* argv[]) {
+	cout >> "hello";
 
 }
