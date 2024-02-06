00b47406ae5aed3002da2b462a14154fcd2a3a69
Meng\m1820 <qm2104@nyu.edu>
2024-02-05 23:13:40 -0500

    no message

--- token.cpp
+++ token.cpp
@@ -1,34 +1,56 @@
 #include<iostream>
-
+#include<fstream>
+#include<cstring>
 using namespace std;
 
 class tokeninfo
 {
 public:
-	tokeninfo();
-	~tokeninfo();
-	char* token[20];
-	int linecount;
-	int offset;
+  tokeninfo();
+  ~tokeninfo();
+  char* token[20];
+  int linecount;
+  int offset;
 private:
 
 };
 
 tokeninfo::tokeninfo()
 {
-	linecount = 0;
-	offset = 0;
+  linecount = 0;
+  offset = 0;
 }
 
 tokeninfo::~tokeninfo()
 {
 }
 
-tokeninfo& getToken(fileobj,tokeninfo &buffer) {
+char* getToken(fstream& fileobj, char* buffer) {
+  static char temp[20];
+  static int linenum = 1;
+  static int lineoffset
+  memset(temp, 0, sizeof(temp));
 
+
+  return temp;
 }
 
 int main(int argc, char* argv[]) {
-	cout >> "hello";
+  fstream file;
+  if (argc <= 1)
+  {
+    cout << "A input file is needed";
+    return 0;
+  }
+  file.open(argv[1]);
+  char tok[20];
+  int linenum = 1;
+  int lineoffset = 1;
+  while ((tok = getToken(file)) != NULL)
+  {
+    printf("token = < % s>  position = % d: % d\n", tok, linenum, lineoffset);
+  }
+  printf(“EOF position % d: % d\n”, linenum, lineoff);
+  cout << "hello";
 
 }
